ecb9959ceabf28205a9eae41ba5448c211883541
gosupraneeth <praneethgosu322@gmail.com>
2021-03-31 23:01:14 +0530

    added best responsive design and basic login andd signup

--- MovieReview/users/urls.py
+++ MovieReview/users/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path("",views.home,name='home'),
+    path('login',views.login_view,name='login'),
+    path('signup',views.signup,name='signup'),
+    path('loginindex',views.loginindex,name='loginindex'),
+    path('logout',views.logout_view,name='logout'),
 ]
